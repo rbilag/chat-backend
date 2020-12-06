3eabdaa22a7dbd996bbdcb1e6431c6ba6aa5b581
Rose <roseb1996@gmail.com>
2020-12-06 19:49:09 +0800

    initialization

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+export enum ChatEvent {
+  CONNECT = 'connect',
+  DISCONNECT = 'disconnect',
+  MESSAGE = 'message',
+}
+export enum MessageStatus {
+  SENT = 'sent',
+  DELIVERED = 'delivered',
+  SEEN = 'seen',
+  UNSENT = 'unsent',
+  ERROR = 'error',
+}
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface ChatMessage {
+  author: string;
+  message: string;
+  timestamp: Date;
+  status: string;
+}
